4261c788d383a4e7595c507c2c1d0adcc83b3d85
Jonathon <jonathonvanausdal257@gmail.com>
2022-08-04 00:01:36 -0600

    Fiveday forecast

--- assets/script.js
+++ assets/script.js
@@ -29,25 +29,71 @@ function getlatlon(apiUrl) {
         });
 
 };
-https: //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}//
-    function getWeather(lonlatdata) {
-        apiLat = Number(lonlatdata[0].lat);
-        apiLon = Number(lonlatdata[0].lon);
-        var weatherApi = "https://api.openweathermap.org/data/2.5/weather?lat=" + apiLat + "&lon=" + apiLon + "&appid=b691141e61134e390bbe0f3d737ad24e";
-        var weatherData = fetch(weatherApi)
-            .then(function(response) {
-                if (response.ok) {
-                    var firstResponse = response.json();
-                    firstResponse.then(
-                            (data) => {
-                                console.log(data);
-                            })
-                        .catch(err => console.log(err))
-                }
-            });
+
+// https: //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}//
+function getWeather(lonlatdata) {
+    apiLat = Number(lonlatdata[0].lat);
+    apiLon = Number(lonlatdata[0].lon);
+    var weatherApi = "https://api.openweathermap.org/data/2.5/weather?lat=" + apiLat + "&lon=" + apiLon + "&units=imperial&appid=b691141e61134e390bbe0f3d737ad24e";
+    var weatherData = fetch(weatherApi)
+        .then(function(response) {
+            if (response.ok) {
+                var firstResponse = response.json();
+                firstResponse.then(
+                        (data) => {
+                            showWeather(data);
+                            fiveday(apiLat, apiLon);
+                        })
+                    .catch(err => console.log(err))
+            }
+        });
+};
+
+function fiveday(apiLat, apiLon) {
+    var fivedayapi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + apiLat + "&lon=" + apiLon + "&appid=b691141e61134e390bbe0f3d737ad24e";
+    var weatherData = fetch(fivedayapi)
+        .then(function(response) {
+            if (response.ok) {
+                var firstResponse = response.json();
+                firstResponse.then(
+                        (data) => {
+                            fivedayforecast(data);
+                            console.log(data);
+                        })
+                    .catch(err => console.log(err))
+            }
+        });
+
+};
+
+function fivedayforecast(data) {
+    // var length = data.list.length;
+    for (var i = 0; i < 5; i++) {
+        var temperature = document.getElementById("temp-" + i);
+        var wind = document.getElementById("wind-" + i);
+        var humidity = document.getElementById("hum-" + i);
+        var date = document.getElementById("date-" + i);
+        temperature.textContent = data.list[i].main.temp + " F";
+        wind.textContent = data.list[i].wind.speed + " MPH";
+        humidity.textContent = data.list[i].main.humidity + " %";
+        date.textContent = data.list[i].dt_txt + " Date";
 
     };
+};
 
-function showWeather() {
+function showWeather(data) {
+    var tempr = document.getElementById("temp");
+    var winds = document.getElementById("wind");
+    var humid = document.getElementById("humidity");
+    var cityy = document.getElementById("city");
 
-}
+    tempr.textContent = data.main.temp + " F";
+    winds.textContent = data.wind.speed + " MPH";
+    humid.textContent = data.main.humidity + " %";
+    cityy.textContent = inputValue;
+
+    console.log(data.main.temp);
+
+
+
+};
